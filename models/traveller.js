6fe0fa40784058b3bf78bb2413139f3c2b8daacf
Illyriat <j4m3s.robson@gmail.com>
2022-10-19 22:04:31 +0100

    first commit

---
+++ models/traveller.js
@@ -0,0 +1,42 @@
+const Journey = require("./journey");
+
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => journey.endLocation);
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => journey.transport === transport);
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  // const getJourneysByMinDistance = this.journeys.filter((journey) => {
+    //   return journey.distance > minDistance;
+    // });
+    // return getJourneysByMinDistance;
+    return this.journeys.filter((journey) => journey.distance > minDistance);
+  };
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  // map. get all transport returned in an array.
+  const transports = this.journeys.map((journey) => journey.transport);
+  ///
+};
+
+
+module.exports = Traveller;
